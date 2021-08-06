3837d6a96876127b8113a28fc82ed3f290af23d5
BBFallen20 <blackbloodfallen123@gmail.com>
2021-08-06 14:49:54 +0300

    Routes refactored.

--- api/apps/profiles/urls.py
+++ api/apps/profiles/urls.py
@@ -1,17 +1,16 @@
 from allauth.account.views import ConfirmEmailView
-from django.conf.urls import url
 from django.urls import include, path
 from rest_auth.registration.views import VerifyEmailView
 from rest_auth.views import PasswordResetConfirmView
 
 
 urlpatterns = [
-    url(r'^', include('rest_auth.urls')),
+    path('', include('rest_auth.urls')),
     path(
         'registration/account-confirm-email/<str:key>/',
         ConfirmEmailView.as_view(),
     ),
-    url(r'^registration/', include('rest_auth.registration.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
     path(
         'account-confirm-email/',
         VerifyEmailView.as_view(),
--- api/settings/urls.py
+++ api/settings/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/film_api/', include('api.apps.films.urls')),
-    path('profiles/', include('api.apps.profiles.urls'))
+    path('api/profiles/', include('api.apps.profiles.urls'))
 ]
